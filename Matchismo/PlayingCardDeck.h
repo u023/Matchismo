1e027a0dc0b8de7ac50f61ee155658d45de2f8dd
Yong Lim <yonglim@microsoft.com>
2016-05-20 22:21:10 -0400

    Add PlayingCardDeck

---
+++ Matchismo/PlayingCardDeck.h
@@ -0,0 +1,14 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by yonglim on 5/20/16.
+//  Copyright © 2016 Rina. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
---
+++ Matchismo/PlayingCardDeck.m
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.m
+//  Matchismo
+//
+//  Created by yonglim on 5/20/16.
+//  Copyright © 2016 Rina. All rights reserved.
+//
+
+#import "PlayingCardDeck.h"
+
+@implementation PlayingCardDeck
+
+@end
