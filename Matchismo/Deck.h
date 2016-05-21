1bfb0b0b287a6769041050e09c566940b0250744
Yong Lim <yonglim@microsoft.com>
2016-05-20 21:58:33 -0400

    Add Deck model

--- Matchismo/Deck.h
+++ Matchismo/Deck.h
@@ -11,8 +11,8 @@
 
 @interface Deck : NSObject
 
-- (void)addCard:(Card *card) atTop:(BOOL)atTop;
-- (void)addCard:(Card *card);
+- (void)addCard:(Card *)card atTop:(BOOL)atTop;
+- (void)addCard:(Card *)card;
 
 - (Card *)drawRandomCard;
 
