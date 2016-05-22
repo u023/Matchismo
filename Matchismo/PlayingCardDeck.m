aa5417d64c5627874c943b345fc23cc81603127c
Yong Lim <yonglim@microsoft.com>
2016-05-22 07:34:16 -0400

    PlayingCardDeck added

--- Matchismo/PlayingCardDeck.m
+++ Matchismo/PlayingCardDeck.m
@@ -7,7 +7,26 @@
 //
 
 #import "PlayingCardDeck.h"
+#import "PlayingCard.h"
 
 @implementation PlayingCardDeck
+
+- (instancetype)init
+{
+    self = [super init];
+    
+    if (self) {
+        for (NSString *suit in [PlayingCard validSuites]) {
+            for (NSUInteger rank = 1; rank <= [PlayingCard maxRank]; rank++) {
+                PlayingCard *card = [[PlayingCard alloc] init];
+                card.rank = rank;
+                card.suit = suit;
+                [self addCard:card];
+            }
+        }
+    }
+    
+    return self;
+}
 
 @end
