58364976fb90b94b0e6e934a6798ca48ff6f2ec7
Yong Lim <yonglim@microsoft.com>
2016-05-26 22:46:34 -0400

    add main logic of this game

--- Matchismo/CardMatchingGame.m
+++ Matchismo/CardMatchingGame.m
@@ -42,9 +42,37 @@
     return self;
 }
 
+static const int MISMATCH_PENALTY = 2;
+static const int MATCH_BONUS = 4;
+static const int COST_TO_CHOOSE = 1;
+
 - (void)chooseCardAtIndex:(NSUInteger)index
 {
     Card *card = [self cardAtIndex:index];
+    
+    if (!card.isMatched) {
+        if (card.isChosen) {
+            card.chosen = NO;
+        } else {
+            // match against other chosen cards
+            for (Card *otherCard in self.cards) {
+                if (otherCard.isChosen && !otherCard.isMatched) {
+                    int matchScore = [card match:@[otherCard]];
+                    if (matchScore) {
+                        self.score += matchScore * MATCH_BONUS;
+                        otherCard.matched = YES;
+                        card.matched = YES;
+                    } else {
+                        self.score -= MISMATCH_PENALTY;
+                        otherCard.chosen = NO;
+                    }
+                    break; // can only choose 2 cards for now
+                }
+            }
+            self.score -= COST_TO_CHOOSE;
+            card.chosen = YES;
+        }
+    }
 }
 
 - (Card *)cardAtIndex:(NSUInteger)index
